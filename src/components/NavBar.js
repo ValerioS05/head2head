362bee49c3d1f6d98a462f2a8f9c7a6c5dd2dc46
Valerie Solinas <valerius92@live.it>
2025-01-15 18:00:31 +0000

    Add basic style and fonts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top" className="navbar">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
-          <img src={logo} alt="logo" height="40"/>
+          <img src={logo} alt="logo" height="40" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <i className="fas fa-bars"></i>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <Nav.Link>
-              <i className="fas fa-home"></i>Home
+              <i className="fas fa-home">Home</i>
             </Nav.Link>
             <Nav.Link>
-              <i className="fas fa-door-open"></i>Sign in
+              <i className="fas fa-door-open">Sign in</i>
             </Nav.Link>
             <Nav.Link>
-              <i className="fas fa-user-check"></i>Sign up
+              <i className="fas fa-user-check">Sign up</i>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
