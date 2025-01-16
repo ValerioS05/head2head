389110f6c8f66b06bf4b6a27d3f63e0a7ef78f18
Valerie Solinas <valerius92@live.it>
2025-01-16 14:15:43 +0000

    Add routing for home/signin/signup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,28 +2,31 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="40" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="40" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <i className="fas fa-bars"></i>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink exact to="/" activeClassName={styles.Active}>
               <i className="fas fa-home">Home</i>
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signin" activeClassName={styles.Active}>
               <i className="fas fa-door-open">Sign in</i>
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signup" activeClassName={styles.Active}>
               <i className="fas fa-user-check">Sign up</i>
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
