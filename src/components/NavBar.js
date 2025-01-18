1843d2bfa8f534f1eef4b1dadcb96ba969643b40
Valerie Solinas <valerius92@live.it>
2025-01-18 15:44:10 +0000

    Create function to toggle navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,14 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import ProfileImage from "./ProfileImage";
 import axios from "axios";
+import useClickOutsideToogle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
   const [profileData, setProfileData] = useState(null);
+  
+  const {expanded, setExpanded, ref} = useClickOutsideToogle();
 
   const handleSignOut = async () => {
     try {
@@ -70,7 +73,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -78,7 +81,7 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addProductIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
