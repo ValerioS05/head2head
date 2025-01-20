10e0f7bc321bb97bf151c2cd759fb0d7a927dee5
Valerie Solinas <valerius92@live.it>
2025-01-20 09:36:21 +0000

    Update Products pages/Display correct images

--- src/pages/products/Product.js
+++ src/pages/products/Product.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+
+import ProfileImage from '../../components/ProfileImage';
+
 // Func Component to display  a product
 const Product = (props) => {
     // Destructured props 
@@ -19,6 +22,8 @@ const Product = (props) => {
         created_at,
         keywords,
         productPage,
+        profile_id,
+        profile_picture,
     } = props;
 
     const currentUser = useCurrentUser();
@@ -27,7 +32,8 @@ const Product = (props) => {
     <Card>
         <Card.Body>
             <Media className='align-items-center justify-content-between'>
-                <Link to={`/profiles/${owner}`}>
+                <Link to={`/profiles/${profile_id}`}>
+                <ProfileImage src={profile_picture} height={55} />
                     {owner}
                 </Link>
                 <div className="d-flex align-items-center">
